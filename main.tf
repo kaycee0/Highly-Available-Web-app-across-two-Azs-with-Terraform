d11ec764e5e93221c0f6eb58532dfcc530170b81
Kenechukwu Eloji <kenechukwu.eloji@Kenechukwus-MacBook-Pro.local>
2026-05-25 19:34:56 +0100

    Adding Route table associations

--- main.tf
+++ main.tf
@@ -122,3 +122,25 @@ resource "aws_route_table" "private_2" {
     Name = "${var.project_name}-private-rt-2"
   }
 }
+
+### Route Table Associations
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public[0].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public[1].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private[0].id
+  route_table_id = aws_route_table.private_1.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private[1].id
+  route_table_id = aws_route_table.private_2.id
+}
